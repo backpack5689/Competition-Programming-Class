174b57a0dbf09c3b0b427becba39570f4cb2f96d
Aaron Thompson <aaron.thompson1889@outlook.com>
2019-02-20 11:43:19 -0600

    Initial
    Created a new assignment folder

---
+++ Backtracking - Aaron/Runner.bat
@@ -0,0 +1,9 @@
+ECHO off
+cls
+python Geppetto.py 
+REM python Geppetto.py <geppetto.2.in >geppetto2t.ans
+REM python Geppetto.py <geppetto.3.in >geppetto3t.ans
+REM FC geppetto1t.ans geppetto.1.ans 
+REM FC geppetto2t.ans geppetto.2.ans
+REM FC geppetto3t.ans geppetto.3.ans
+pause
