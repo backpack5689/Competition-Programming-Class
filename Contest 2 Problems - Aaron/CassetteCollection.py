d717f7e4b36ec41ad34b2e69360d27d551dc688d
Aaron Thompson <aaron.thompson1889@outlook.com>
2019-03-06 19:21:11 -0600

    CassetteCollection Final
    Completed CassetteCollection. Takes a couple seconds to run, and a little bit to File Compare in the batch.

--- Contest 2 Problems - Aaron/CassetteCollection.py
+++ Contest 2 Problems - Aaron/CassetteCollection.py
@@ -1,46 +1,43 @@
 #Fenwick Class
 def getSum(BITTree,i): 
 	s = 0
-	i = i+1
+	#i = i+1
 	while i > 0: 
 		s += BITTree[i] 
 		i -= i & (-i) 
 	return s 
 def updateBit(BITTree , n , i ,v): 
-	i += 1
+	#i += 1
 	while i <= n: 
 		BITTree[i] += v 
 		i += i & (-i) 
 def construct(arr, n): 
 	BITTree = [0]*(n+1) 
 	for i in range(n): 
-		updateBit(BITTree, n, i, arr[i]) 
+		updateBit(BITTree, n, i+1, arr[i]) 
 	return BITTree 
-		
-		
+
+#---------------------------------------------------------------------------	
+
 repetitions = int(input())
 for _ in range(repetitions):
 	numCassettes, numOperations = [int(x) for x in input().split()]
-	positionArray = [int(x) for x in reversed(range(numCassettes))]
-	#If numCassettes was at 5, position array would look like {4,3,2,1,0}
+	positionArray = [int(x)+1 for x in reversed(range(numCassettes))]
 	boolArray = [1] * numCassettes
-	boolArray.extend([0] * numOperations)
-	#Operations to follow
+	boolArray.extend([0] * (numOperations+1))
 	myFW = construct(boolArray,len(boolArray))
-	
 	operationList = [int(x) for x in input().split()]
-	operationsToComplete = len(operationList)-1
+	operationsToComplete = len(operationList)
+	endPos = numCassettes+1
 	for operation in operationList:
 		operationsToComplete -= 1
 		startPos = positionArray[operation-1]
-		endPos = max(positionArray)+1
-		numCassettesSkipped = getSum(myFW, endPos) - getSum(myFW, startPos)
-		boolArray[startPos] = 0
-		boolArray[endPos] = 1
+		endPos = endPos+1
+		updateBit(myFW, len(boolArray), startPos, -1)
 		updateBit(myFW, len(boolArray), endPos, 1)
-		updateBit(myFW, len(boolArray), startPos, 0)
+		numCassettesSkipped = getSum(myFW, endPos-1) - getSum(myFW, startPos)
 		positionArray[operation-1] = endPos
-		if operationsToComplete < 0:
-			print(numCassettes)
+		if operationsToComplete == 0:
+			print(numCassettesSkipped)
 		else:
 			print(numCassettesSkipped, end = " ")
