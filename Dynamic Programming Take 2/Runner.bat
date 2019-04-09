33c7abdc7fc78c1ceb382006f849c973423b9e93
Aaron Thompson <aaron.thompson1889@outlook.com>
2019-04-09 14:53:14 -0500

    Updated Runner

--- Dynamic Programming Take 2/Runner.bat
+++ Dynamic Programming Take 2/Runner.bat
@@ -3,7 +3,16 @@ cls
 Echo Running
 Echo.
 python EditChanges.py <TestData/StringCorrection.in >TestData/StringCorrection.try
-Echo 1/1 Completed
+Echo 1/4 Completed
+Echo.
+REM python x.py <TestData/x.in >Testdata/x.try
+Echo 2/4 Completed
+Echo.
+REM z.py <TestData/z.in >TestData/z.try
+Echo 3/4 Completed
+Echo.
+REM M.py <TestData/M.in >TestData/M.try
+Echo 4/4 Completed
 Echo.
 
 Echo Comparing
